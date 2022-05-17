8f9cfcc1f76870d3e294fe03683beba0514d9c5c
Muhammad Hidayatullah <muhammadhidayatullah@192.168.1.17>
2022-05-18 01:06:54 +0800

    revisi

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,23 @@ module.exports = {
       fontFamily: {
         'script': 'Playball',
         'oleo':'Oleo Script Swash Caps'
+      },
+      screens: {
+        'xxs': '340px', // min-width
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     },
     
